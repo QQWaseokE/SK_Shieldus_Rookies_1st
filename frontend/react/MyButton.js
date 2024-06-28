b45d1fcf65914cfbfba88cd53d92a97b5bfa6be8
QQWaseokE <waseoke@gmail.com>
2024-06-28 14:16:28 +0900

    Update MyButton.js

--- frontend/react/MyButton.js
+++ frontend/react/MyButton.js
@@ -1,11 +1,11 @@
 function MyButton(props) {
-    const [isClicked, setIsClicked] = React.useState(false);
+    const [isClicked, setIsClicked] = React.useState(false);        // useState : 상태변수를 정의
 
     return React.createElement(
         "button", 
-        { onClick: () => setIsClicked(!isClicked) }, 
+        { onClick: () => setIsClicked(!isClicked) },                //button이 어떻게 동작해야 하는지
         isClicked ? '클릭되었습니다.' : '클릭해 주세요.'
-    )
+    )                                                               // <button onclick = " ... "> 클릭xxx </button>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));  // <div id="root"></div>
