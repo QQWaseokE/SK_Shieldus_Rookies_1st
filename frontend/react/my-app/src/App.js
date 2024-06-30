216882c993d116744da024f028448df090b13a6f
QQWaseokE <waseoke@gmail.com>
2024-06-30 21:14:10 +0900

    JSX 안에서는 자바스크립트 표현식을 {}로 감싸서 작성
    6/28(금)

--- frontend/react/my-app/src/App.js
+++ frontend/react/my-app/src/App.js
@@ -3,25 +3,13 @@ import './App.css';
 // import { Fragment } from 'react';
 
 function App() {
+  const name = '홍길동';
+  const number = 2;
+
   return (
     <>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-      <h1>리액트 앱</h1>
+      <h1>안녕, {name}</h1>   
+      <h2>{number + 2}</h2>
     </>
   );
 }
