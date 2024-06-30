f6ee59638eeb8192b4cae43b7367a4208fa654b5
QQWaseokE <waseoke@gmail.com>
2024-06-30 21:21:05 +0900

    조건부 렌더링, && 연산자
    6/28(금)

--- frontend/react/my-app/src/App.js
+++ frontend/react/my-app/src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 // import { Fragment } from 'react';
 
 function App() {
-  const name = '홍길동x';
+  const name = '홍길동';
 
   return (
     <>
-      { name === '홍길동' ? '환영합니다.' : null }		
-    </>									
+      { name === '홍길동' && '환영합니다.' }
+    </>
   );
 }
 
