ef2567c7078c9b6c5c9dd5e941ce185d74a336a7
QQWaseokE <waseoke@gmail.com>
2024-06-30 21:58:58 +0900

    함수형 컴포넌트를 작성
    6/28(금)

--- frontend/react/my-app/src/App.js
+++ frontend/react/my-app/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import MyComponentClass from './MyComponentClass';
+import MyComponentFunction from './MyComponentFunction';
 
 function App() {
   return (
     <>
       <MyComponentClass />
+      <hr/> 
+      <MyComponentFunction />
     </>
   );  
 }
