02554e435b5bd65aa3f365c8a69376973d3967d9
QQWaseokE <waseoke@gmail.com>
2024-06-30 21:44:33 +0900

    CSS 클래스를 사용할 때 class가 아닌 className으로 설정
    6/28(금)

--- frontend/react/my-app/src/App.js
+++ frontend/react/my-app/src/App.js
@@ -1,15 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-// import { Fragment } from 'react';
 
 function App() {
   const name = '홍길동';
   return (
     <>
-      <div style={{backgroundColor: 'black', color: 'yellow', fontSize: 48, padding: '16px'}}>
+      <div className="react">
         {name}
       </div>
-    </>
+    </>				   
   );
 }
 
