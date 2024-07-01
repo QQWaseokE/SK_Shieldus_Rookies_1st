daede0134047390ea51566a5877bd5d4b9294c27
QQWaseokE <waseoke@gmail.com>
2024-07-02 00:41:16 +0900

    클래스형 컴포넌트로 변경
    7/1(월)

--- frontend/react/my-app/src/App.js
+++ frontend/react/my-app/src/App.js
@@ -1,25 +1,31 @@
 import './App.css';
-import {useState} from 'react';
 
-function Clock() {
-  const [now,setNow] = useState(new Date().toLocaleTimeString());
-
-  setInterval(() => {
-    setNow(new Date().toLocaleTimeString());
-  },1000);
-  return (
-    <>
-      <h1>현재 시간 : { now }</h1>
-    </>
-  );
+class Clock extends Component {
+  state = {
+    now: new Date().toLocaleTimeString()
+  };
+  
+  render() {
+    setInterval(() => {
+      this.setState({ now: new Date().toLocaleTimeString() });
+    }, 1000);
+  
+    return (
+      <>
+        <h1>현재 시간 : {this.state.now}</h1>
+      </>
+    );
+  }
 }
 
-function App() {
-  return (
-    <>
-      <Clock />
-    </>
-  );  
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Clock />
+      </>
+    );  
+  }
 }
 
 export default App;
