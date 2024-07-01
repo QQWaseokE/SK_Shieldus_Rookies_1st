eca097873104f664f52fbc2bdbf57d64ba1aa24d
QQWaseokE <waseoke@gmail.com>
2024-07-01 23:00:24 +0900

    댓글 데이터를 이용하도록 수정
    7/1(월)

--- frontend/react/my-app/src/comment/CommentList.js
+++ frontend/react/my-app/src/comment/CommentList.js
@@ -1,11 +1,30 @@
 import Comment from "./Comment";
 
+// 댓글 데이터
+const comments = [
+    {name:"홍길동", comment:"안녕하세요. 홍길동입니다."},
+    {name:"홍길동", comment:"안녕하세요. 홍길동입니다."},
+    {name:"홍길동", comment:"안녕하세요. 홍길동입니다."},
+];
+
 function CommentList(props) {
     return (
         <>
-            <Comment name="홍길동" comment="안녕하세요. 홍길동입니다."/>
-            <Comment name="고길동" comment="안녕하세요. 고길동입니다."/>
-            <Comment name="신길동" comment="안녕하세요. 신길동입니다."/>
+            {   // 익명 함수 표현식
+                comments.map(function(c) { 
+                    return <Comment name={c.name} comment={c.comment}/>
+            })
+            }
+
+            {   // 화살표 함수
+                comments.map((c) => {
+                    return <Comment name={c.name} comment={c.comment}/>;
+            })
+            }
+
+            {   // 화살표 함수 (축약)
+                comments.map(c => <Comment name={c.name} comment={c.comment}/>)
+            }
         </>
     );
 }
