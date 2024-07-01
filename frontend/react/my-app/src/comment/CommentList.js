9cd7fd8c3204a9868fc500c8aef831a5b322e3af
QQWaseokE <waseoke@gmail.com>
2024-07-01 23:02:43 +0900

    key가 누락되었을 때 경고
    7/1(월)

--- frontend/react/my-app/src/comment/CommentList.js
+++ frontend/react/my-app/src/comment/CommentList.js
@@ -2,28 +2,28 @@ import Comment from "./Comment";
 
 // 댓글 데이터
 const comments = [
-    {name:"홍길동", comment:"안녕하세요. 홍길동입니다."},
-    {name:"홍길동", comment:"안녕하세요. 홍길동입니다."},
-    {name:"홍길동", comment:"안녕하세요. 홍길동입니다."},
+    { name: "홍길동", comment: "안녕하세요. 홍길동입니다." },
+    { name: "홍길동", comment: "안녕하세요. 홍길동입니다." },
+    { name: "홍길동", comment: "안녕하세요. 홍길동입니다." },
 ];
 
 function CommentList(props) {
     return (
         <>
             {   // 익명 함수 표현식
-                comments.map(function(c) { 
-                    return <Comment name={c.name} comment={c.comment}/>
-            })
+                comments.map(function (c, index) {
+                    return <Comment name={c.name} comment={c.comment} key={index} />
+                })
             }
 
             {   // 화살표 함수
-                comments.map((c) => {
-                    return <Comment name={c.name} comment={c.comment}/>;
-            })
+                comments.map((c, index) => {
+                    return <Comment name={c.name} comment={c.comment} key={index} />;
+                })
             }
 
             {   // 화살표 함수 (축약)
-                comments.map(c => <Comment name={c.name} comment={c.comment}/>)
+                comments.map((c, index) => <Comment name={c.name} comment={c.comment} key={index} />)
             }
         </>
     );
