ea4170759244d32b918ab3a3560d982900970d05
QQWaseokE <waseoke@gmail.com>
2024-07-01 22:30:33 +0900

    댓글 컴포넌트
    7/1(월)

--- frontend/react/my-app/src/App.js
+++ frontend/react/my-app/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-import TrafficLight from './TrafficLight';
+import CommentList from './comment/CommentList';
 
 function App() {
-  const tlSize = 100;
-  const tlColors = [ "red", "blue", "green", "yellow" ];
-
+  
   return (
     <>
-      <TrafficLight size={tlSize} colors={tlColors} />      
+      <CommentList />      
     </>
   );  
 }
