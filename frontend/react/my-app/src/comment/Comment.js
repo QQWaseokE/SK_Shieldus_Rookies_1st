13ecc4b31a13e0d6a23af6e59a0691db272cb078
QQWaseokE <waseoke@gmail.com>
2024-07-01 22:43:53 +0900

    Comment 컴포넌트에 스타일을 추가
    7/1(월)

--- frontend/react/my-app/src/comment/Comment.js
+++ frontend/react/my-app/src/comment/Comment.js
@@ -1,8 +1,39 @@
 function Comment(props) {
+    const styles = {
+        wrapper: {
+            display: 'flex', flexDirection: 'row', border: '1px solid gray', borderRadius: 16, padding: 8, margin: 8
+        },
+        image: {
+            width: 50, height: 50, borderRadius: 25
+        },
+        contentContainer: {
+            marginLeft: 10,
+            display: 'flex', flexDirection: 'column'
+        },
+        nameText: {
+            color: 'black',
+            fontSize: 16,
+            fontWeight: 'bold',
+            marginBottom: 3
+        },
+        commentText: {
+            color: 'black',
+            fontSize: 16
+        }
+    };
     return (
-        <>
-            <h1>댓글 컴포넌트</h1>
-        </>
+        <div style={styles.wrapper}>
+            { /*작성한 사람의 이미지*/}
+            <div>
+                <img style={styles.image}
+                    src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png" />
+            </div>
+            { /*작성한 사람의 이름과 댓글 내용*/}
+            <div style={styles.contentContainer}>
+                <span style={styles.nameText}>작성자 이름</span>
+                <span style={styles.commentText}>댓글 내용</span>
+            </div>
+        </div>
     );
 }
 export default Comment;
