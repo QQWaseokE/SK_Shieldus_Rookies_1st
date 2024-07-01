56e9d0cc89e41cb510c007d64fdc5d1124e54c97
QQWaseokE <waseoke@gmail.com>
2024-07-01 23:47:56 +0900

    LAB 사용자 정보에 주민번호가 전달되었을 때 남, 여 구문을 이름 뒤에 출력하도록 수정
    7/1(월)

--- frontend/react/my-app/src/comment/CommentList.js
+++ frontend/react/my-app/src/comment/CommentList.js
@@ -9,23 +9,37 @@ const comments = [
 
 // 사용자 데이터
 const users = [
-    { name: "홍길동", picture: "https://png.pngtree.com/png-clipart/20190705/original/pngtree-vector-business-men-icon-png-image_4186858.jpg" }, 
-    { name: "고길동", picture: "https://png.pngtree.com/png-clipart/20190520/original/pngtree-male-worker-icon-graphic-png-image_3668949.jpg" }, 
-    { name: "신길동", picture: "https://png.pngtree.com/png-clipart/20190630/original/pngtree-vector-avatar-icon-png-image_4162757.jpg" }, 
+    { name: "홍길동", regno: "701010-1457934", picture: "https://png.pngtree.com/png-clipart/20190705/original/pngtree-vector-business-men-icon-png-image_4186858.jpg" },
+    { name: "신길동", regno: "201010-3457934", picture: "https://png.pngtree.com/png-clipart/20190630/original/pngtree-vector-avatar-icon-png-image_4162757.jpg" },
+    { name: "고길동", regno: "211010-4157934", picture: "https://png.pngtree.com/png-clipart/20190520/original/pngtree-male-worker-icon-graphic-png-image_3668949.jpg" }
 ];
 
-function getUser(name) {
-    return users.filter(u => u.name === name);
-}
 
 function CommentList(props) {
+    // 성별을 반환하는 함수를 정의
+    const getUserGender = name => {
+        const user = users.find(user => user.name === name);
+        return [1, 3].includes(Number(user.regno[7])) ? "남" : "여";    // include() : 배열에 특정 요소를 포함하는지 확인하는 함수
+    };
+    
+    // 댓글 데이터와 사용자 데이터를 결합하는 과정에서 이름을 재설정
+    const commentsWithUser = comments.map(c => ({ ...c, picture: users.find(u => u.name === c.name).picture, name: `${c.name} (${getUserGender(c.name)})` }));
+    
+
     return (
         <>
             <h1>모두 출력</h1>
             {   
-                comments.map((c, i) => <Comment name={c.name} comment={c.comment} key={i} picture={getUser(c.name)[0].picture} />)
+                commentsWithUser
+                .map((c, i) => <Comment name={c.name} comment={c.comment} key={i} picture={c.picture} />)
             }
-            
+            <hr/>
+            <h1>이름이 홍길동인 경우에만 출력</h1>
+            {
+                commentsWithUser
+                .filter(c => c.name.indexOf('홍길동') >= 0)
+                .map((c, i) => <Comment name={c.name} comment={c.comment} picture={c.picture} key={i} />)
+            }
         </>
     );
 }
