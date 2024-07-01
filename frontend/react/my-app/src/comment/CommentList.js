c01bfa38e1e714df8e465a02aa785a1b206c7135
QQWaseokE <waseoke@gmail.com>
2024-07-01 22:48:38 +0900

    CommentList 컴포넌트에 props 값을 설정해서 Comment 컴포넌트를 호출하도록 수정
    7/1(월)

--- frontend/react/my-app/src/comment/CommentList.js
+++ frontend/react/my-app/src/comment/CommentList.js
@@ -3,9 +3,9 @@ import Comment from "./Comment";
 function CommentList(props) {
     return (
         <>
-            <Comment />
-            <Comment />
-            <Comment />
+            <Comment name="홍길동" comment="안녕하세요. 홍길동입니다."/>
+            <Comment name="고길동" comment="안녕하세요. 고길동입니다."/>
+            <Comment name="신길동" comment="안녕하세요. 신길동입니다."/>
         </>
     );
 }
