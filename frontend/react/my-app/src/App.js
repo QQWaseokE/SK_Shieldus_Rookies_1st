8efafa15d2e00a271c3874ab40292bbd21b0bdf6
QQWaseokE <waseoke@gmail.com>
2024-07-02 00:39:13 +0900

    시계 만들기
    7/1(월)

--- frontend/react/my-app/src/App.js
+++ frontend/react/my-app/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
-import Say from './Say';
+import {useState} from 'react';
+
+function Clock() {
+  const [now,setNow] = useState(new Date().toLocaleTimeString());
+
+  setInterval(() => {
+    setNow(new Date().toLocaleTimeString());
+  },1000);
+  return (
+    <>
+      <h1>현재 시간 : { now }</h1>
+    </>
+  );
+}
 
 function App() {
   return (
     <>
-      <Say />
+      <Clock />
     </>
   );  
 }
