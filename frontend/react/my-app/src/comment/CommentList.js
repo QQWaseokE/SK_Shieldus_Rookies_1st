c87036eec31c5d244cb924fe11a7773719eb6fd9
QQWaseokE <waseoke@gmail.com>
2024-07-01 23:05:26 +0900

    CommentList 컴포넌트의 댓글 데이터에 이미지 정보를 추가하고, Comment 컴포넌트의 props 변수로 전달
    7/1(월)

--- frontend/react/my-app/src/comment/CommentList.js
+++ frontend/react/my-app/src/comment/CommentList.js
@@ -2,28 +2,28 @@ import Comment from "./Comment";
 
 // 댓글 데이터
 const comments = [
-    { name: "홍길동", comment: "안녕하세요. 홍길동입니다." },
-    { name: "홍길동", comment: "안녕하세요. 홍길동입니다." },
-    { name: "홍길동", comment: "안녕하세요. 홍길동입니다." },
+    { name: "홍길동", comment: "안녕하세요. 홍길동입니다.", picture: "https://png.pngtree.com/png-clipart/20190705/original/pngtree-vector-business-men-icon-png-image_4186858.jpg" }, 
+    { name: "고길동", comment: "안녕하세요. 고길동입니다.", picture: "https://png.pngtree.com/png-clipart/20190520/original/pngtree-male-worker-icon-graphic-png-image_3668949.jpg" }, 
+    { name: "신길동", comment: "안녕하세요. 신길동입니다.", picture: "https://png.pngtree.com/png-clipart/20190630/original/pngtree-vector-avatar-icon-png-image_4162757.jpg" }, 
 ];
 
 function CommentList(props) {
     return (
         <>
-            {   // 익명 함수 표현식
-                comments.map(function (c, index) {
-                    return <Comment name={c.name} comment={c.comment} key={index} />
-                })
+            {   // 화살표 함수 (축약)
+                comments.map((c, index) => <Comment name={c.name} comment={c.comment} key={index} picture={c.picture} />)
             }
-
+            <hr/>
             {   // 화살표 함수
                 comments.map((c, index) => {
-                    return <Comment name={c.name} comment={c.comment} key={index} />;
+                    return <Comment name={c.name} comment={c.comment} key={index} picture={c.picture} />;
                 })
             }
-
-            {   // 화살표 함수 (축약)
-                comments.map((c, index) => <Comment name={c.name} comment={c.comment} key={index} />)
+            <hr/>
+            {   // 익명 함수 표현식
+                comments.map(function(c, index) {
+                    return <Comment name={c.name} comment={c.comment} key={index} picture={c.picture} />;
+                })
             }
         </>
     );
