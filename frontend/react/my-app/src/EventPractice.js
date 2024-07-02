06ae908619a7440f4a3117d052bfb4f525c85154
QQWaseokE <waseoke@gmail.com>
2024-07-02 21:27:44 +0900

    Property Intializer Syntax를 사용해서 메서드를 작성
    7/2(화)

--- frontend/react/my-app/src/EventPractice.js
+++ frontend/react/my-app/src/EventPractice.js
@@ -1,26 +1,36 @@
 import { Component } from "react";
 
 class EventPractice extends Component {
-    constructor(props) {
-        super(props);
-        // handlerXXX 메서드를 this와 함께 바인딩한 새로운 메서드로 대체
-        // 클래스 메서드의 this를 클래스 인스턴스로 고정 
-        // 이벤트 핸들러나 콜백 함수에서 메서드가 올바른 클래스 인스턴스를 참조하도록 보장 
-        this.handlerChange = this.handlerChange.bind(this);
-        this.handlerClick = this.handlerClick.bind(this);
-        this.state = {
-            message: ''
-        };
-    }
+    // constructor(props) {
+    //     super(props);
+    //     this.handlerChange = this.handlerChange.bind(this);
+    //     this.handlerClick = this.handlerClick.bind(this);
+    //     this.state = {
+    //         message: ''
+    //     };
+    // }
     
-    handlerChange(e) {
+    // handlerChange(e) {
+    //     console.log(e.target.value);
+    //     this.setState({message: e.target.value});
+    // }
+
+    // handlerClick() {
+    //     this.setState({message: ""})
+    // }
+
+    state = {
+        message: ''
+    };
+
+    // ES6 클래스 필드 문법을 사용해서 메서드를 화살표 함수로 정의 
+    // 화살표 함수는 자신만의 this를 가지지 않고, 선언된 클래스 인스턴스의 this를 유지
+    handlerChange = e => {
         console.log(e.target.value);
         this.setState({message: e.target.value});
-    }
+    };
 
-    handlerClick() {
-        this.setState({message: ""})
-    }
+    handlerClick = () => this.setState({message: ""});
 
     render() {
         return (
