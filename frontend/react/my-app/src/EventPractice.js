71f7ada2e4fb088511a8f738042a37d8674e7933
QQWaseokE <waseoke@gmail.com>
2024-07-02 22:01:48 +0900

    입력창의 이름을 관리하는 state 변수를 추가해서 이벤트 핸들러 함수를 하나로 통일
    7/2(화)

--- frontend/react/my-app/src/EventPractice.js
+++ frontend/react/my-app/src/EventPractice.js
@@ -1,25 +1,35 @@
 import { useState } from "react";
 
 const EventPractice = () => {
-    const [ message, setMessage ] = useState('');
-    const [ username, setUsername ] = useState('');
+    // const [ message, setMessage ] = useState('');
+    // const [ username, setUsername ] = useState('');
 
-    // const handlerChangeMessage = e => setMessage(e.target.value);
-    // const handlerChangeUsername = e => setUsername(e.target.value);
+    const [form, setForm] = useState({
+        message: '',
+        username: ''
+    });
+
+    const { message, username } = form;
 
     const handlerChange = e => {
+        /*
         if (e.target.name === 'message') {
             setMessage(e.target.value);
         }
         if (e.target.name === 'username') {
             setUsername(e.target.value);
         }
+        */
+       setForm({...form, [e.target.name]: e.target.value})
     }
 
     const handlerClick = () => {
+        /*
         setMessage('');
         setUsername('');
-    ;}
+        */
+       setForm({message:'', username:''});
+    }
 
     const handlerKeyUp = e => {
         if (e.key === 'Enter') {
@@ -30,10 +40,10 @@ const EventPractice = () => {
     return (
         <>
             <h1>이벤트 연습</h1>
-            <input type="text" name="message" placeholder="입력해 보세요." 
+            <input type="text" name="message" placeholder="입력해 보세요."
                 value={message} onChange={handlerChange} onKeyUp={handlerKeyUp} />
             <input type="text" name="username" placeholder="사용자 이름을 입력하세요."
-                value={username} onChange={handlerChange} onKeyUp={handlerKeyUp}/>
+                value={username} onChange={handlerChange} onKeyUp={handlerKeyUp} />
             <h2>입력 내용 » {message}</h2>
             <h2>사용자 이름 » {username}</h2>
             <button onClick={handlerClick}>확인</button>
