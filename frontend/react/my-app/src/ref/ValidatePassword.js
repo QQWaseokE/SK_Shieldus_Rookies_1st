804b87ff60f87550860d57b341dee08e2d51ae78
QQWaseokE <waseoke@gmail.com>
2024-07-02 23:08:50 +0900

    패스워드 검증 컴포넌트
    7/2(화)

--- frontend/react/my-app/src/ref/ValidatePassword.js
+++ frontend/react/my-app/src/ref/ValidatePassword.js
@@ -2,16 +2,24 @@ import { Component } from "react";
 
 class ValidatePassword extends Component {
     state = {
-        password: ''
+        password: '',
+        isValid: false
     };
 
-    handlerChangePassword = e => this.setState({password: e.target.value});
-
-    reder() {
+    handlerChangePassword = e => this.setState({ password: e.target.value });
+    handlerButtonClick = () => {
+        if (this.state.password === "0000") {
+            this.setState({ isValid: true });
+        } else {
+            this.setState({ isValid: false });
+        }
+    };
+    render() {
         return (
             <>
-                <input type="password" value={password} onChange={handlerChangePassword} />
-                <button>패스워드 검증</button>
+                <input type="password" value={this.state.password} onChange={this.handlerChangePassword}
+                    style={this.state.isValid ? { backgroundColor: "blue" } : { backgroundColor: "red" }} />
+                <button onClick={this.handlerButtonClick}>패스워드 검증</button>
             </>
         );
     }
