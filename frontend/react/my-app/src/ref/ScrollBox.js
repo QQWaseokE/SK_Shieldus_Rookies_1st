6ed9d839207810c6a0b779b12dcbe88a764d1c7b
QQWaseokE <waseoke@gmail.com>
2024-07-02 23:20:13 +0900

    ScrollBox 컴포넌트에 맨 위로 이동, 맨 아래로 이동 버튼을 추가
    7/2(화)

--- frontend/react/my-app/src/ref/ScrollBox.js
+++ frontend/react/my-app/src/ref/ScrollBox.js
@@ -1,6 +1,18 @@
-import { Component } from "react";
+import React, { Component } from "react";
 
 class ScrollBox extends Component {
+    
+    myscroll = React.createRef();
+
+    scrollTop = () => {
+        this.myscroll.current.scrollTop = 0;
+    };
+
+    scrollBottom = () => {
+        const {scrollHeight, clientHeight} = this.myscroll.current;
+        this.myscroll.current.scrollTop = scrollHeight - clientHeight;
+    };
+
     render() {
         const styles = {
             outer: {
@@ -15,10 +27,17 @@ class ScrollBox extends Component {
                 background: 'linear-gradient(white, black)'
             }
         };
+
         return (
-            <div style={styles.outer}>
-                <div style={styles.inner}></div>
-            </div>
+            <>
+                <div ref={this.myscroll} style={styles.outer}>
+                    <div style={styles.inner}></div>
+                </div>
+                <div>
+                    <button onClick={this.scrollTop}>맨 위로 이동</button>
+                    <button onClick={this.scrollBottom}>맨 아래로 이동</button>
+                </div>
+            </>
         );
     }
 }
