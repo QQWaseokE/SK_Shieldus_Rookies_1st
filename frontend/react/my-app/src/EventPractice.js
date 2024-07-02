e37ad4ecd6d9a1fff219f0487299c28ed57b6385
QQWaseokE <waseoke@gmail.com>
2024-07-02 20:44:07 +0900

    이벤트 핸들러 함수를 미리 정의해서 전달하는 방식으로 변경
    7/2(화)

--- frontend/react/my-app/src/EventPractice.js
+++ frontend/react/my-app/src/EventPractice.js
@@ -1,9 +1,27 @@
 import { Component } from "react";
 
 class EventPractice extends Component {
-    state = {
-        message: ''
-    };
+    constructor(props) {
+        super(props);
+        // handlerXXX 메서드를 this와 함께 바인딩한 새로운 메서드로 대체
+        // 클래스 메서드의 this를 클래스 인스턴스로 고정 
+        // 이벤트 핸들러나 콜백 함수에서 메서드가 올바른 클래스 인스턴스를 참조하도록 보장 
+        this.handlerChange = this.handlerChange.bind(this);
+        this.handlerClick = this.handlerClick.bind(this);
+        this.state = {
+            message: ''
+        };
+    }
+    
+    handlerChange(e) {
+        console.log(e.target.value);
+        this.setState({message: e.target.value});
+    }
+
+    handlerClick() {
+        this.setState({message: ""})
+    }
+
     render() {
         return (
             <>
@@ -11,12 +29,9 @@ class EventPractice extends Component {
                 <input type="text" name="message" 
                     placeholder="입력해 보세요." 
                     value={this.state.message}
-                    onChange={e => {
-                        console.log(e.target.value);
-                        this.setState({message: e.target.value});
-                    }} />
+                    onChange={this.handlerChange} />
                 <h2>입력 내용 » {this.state.message}</h2>
-                <button onClick={() => this.setState({message: ""})}>확인</button>
+                <button onClick={this.handlerClick}>확인</button>
             </>
         );
     }
