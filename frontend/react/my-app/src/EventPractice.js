63fbee5807a078993badd05134b92b1101ab55ae
QQWaseokE <waseoke@gmail.com>
2024-07-02 20:29:32 +0900

    버튼을 추가하고, 해당 버튼을 클릭하면 입력값과 입력 내용을 제거하도록 수정
    7/2(화)

--- frontend/react/my-app/src/EventPractice.js
+++ frontend/react/my-app/src/EventPractice.js
@@ -1,13 +1,22 @@
 import { Component } from "react";
 
 class EventPractice extends Component {
+    state = {
+        message: ''
+    };
     render() {
         return (
             <>
                 <h1>이벤트 연습</h1>
                 <input type="text" name="message" 
-                placeholder="입력해 보세요." 
-                onChange={e => console.log(e.target.value)} />
+                    placeholder="입력해 보세요." 
+                    value={this.state.message}
+                    onChange={e => {
+                        console.log(e.target.value);
+                        this.setState({message: e.target.value});
+                    }} />
+                <h2>입력 내용 » {this.state.message}</h2>
+                <button onClick={() => this.setState({message: ""})}>확인</button>
             </>
         );
     }
