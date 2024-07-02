4fa5a93f0c0815793f2eee9b93757dc2081c7b92
QQWaseokE <waseoke@gmail.com>
2024-07-02 22:44:52 +0900

    상태 내리기
    7/2(화)

--- frontend/react/my-app/src/App.js
+++ frontend/react/my-app/src/App.js
@@ -1,13 +1,40 @@
-import { Component } from 'react';
-import IterationSample from './IterationSample';
+import { Component, useState } from 'react';
+
+const Parent = () => {
+  const [ no, setNo ] = useState(0);
+  return (
+    <>
+      <h1>입력 값: {no}</h1>
+      <input type="number" value={no} onChange={e => setNo(e.target.value)}/>
+      <ChildA no={no} />
+      <ChildB no={no} />    
+    </>
+  )
+};
+
+const ChildA = ({no}) => {
+  return (
+    <>
+      <h1>2를 곱한 값: {no * 2}</h1>
+    </>
+  );
+};
+
+const ChildB = ({no}) => {
+  return (
+    <>
+      <h1>3를 곱한 값: {no * 3 }</h1>
+    </>
+  );
+};
 
 class App extends Component {
   render() {
     return (
       <>
-        <IterationSample />
+        <Parent />          
       </>
-    );  
+    );
   }
 }
 
