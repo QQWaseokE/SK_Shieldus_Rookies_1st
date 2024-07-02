fb09f331a052bd53f82ea4893314cb633a7358c9
QQWaseokE <waseoke@gmail.com>
2024-07-02 22:12:00 +0900

    컴포넌트 반복
    7/2(화)

--- frontend/react/my-app/src/App.js
+++ frontend/react/my-app/src/App.js
@@ -1,11 +1,11 @@
 import { Component } from 'react';
-import EventPractice from './EventPractice';
+import IterationSample from './IterationSample';
 
 class App extends Component {
   render() {
     return (
       <>
-        <EventPractice />
+        <IterationSample />
       </>
     );  
   }
