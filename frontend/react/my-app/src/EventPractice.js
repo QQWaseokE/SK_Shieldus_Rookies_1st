2a49da6cd21848100c17e287b5d809d18d3f34ad
QQWaseokE <waseoke@gmail.com>
2024-07-02 20:17:40 +0900

    EventPractice 컴포넌트에 입력창을 추가하고, 입력값을 콘솔에 출력하도록 수정
    7/2(화)

--- frontend/react/my-app/src/EventPractice.js
+++ frontend/react/my-app/src/EventPractice.js
@@ -5,6 +5,9 @@ class EventPractice extends Component {
         return (
             <>
                 <h1>이벤트 연습</h1>
+                <input type="text" name="message" 
+                placeholder="입력해 보세요." 
+                onChange={e => console.log(e.target.value)} />
             </>
         );
     }
