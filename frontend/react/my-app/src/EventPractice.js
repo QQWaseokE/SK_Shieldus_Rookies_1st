7e84a74259cb82a99f7156f8d10d0adbd054c7bc
QQWaseokE <waseoke@gmail.com>
2024-07-02 21:50:44 +0900

    함수형 컴포넌트에서 이벤트 처리
    7/2(화)

--- frontend/react/my-app/src/EventPractice.js
+++ frontend/react/my-app/src/EventPractice.js
@@ -1,36 +1,36 @@
-import { Component } from "react";
+import { useState } from "react";
 
-class EventPractice extends Component {
-    state = {
-        message: '',
-        username: ''
+const EventPractice = () => {
+    const [ message, setMessage ] = useState('');
+    const [ username, setUsername ] = useState('');
+
+    const handlerChangeMessage = e => setMessage(e.target.value);
+    const handlerChangeUsername = e => setUsername(e.target.value);
+
+    const handlerClick = () => {
+        setMessage('');
+        setUsername('');
+    ;}
+
+    const handlerKeyUp = e => {
+        if (e.key === 'Enter') {
+            handlerClick();
+        }
     };
 
-    handlerChange = e => this.setState({[e.target.name]: e.target.value});
+    return (
+        <>
+            <h1>이벤트 연습</h1>
+            <input type="text" name="message" placeholder="입력해 보세요." 
+                value={message} onChange={handlerChangeMessage} onKeyUp={handlerKeyUp} />
+            <input type="text" name="username" placeholder="사용자 이름을 입력하세요."
+                value={username} onChange={handlerChangeUsername} onKeyUp={handlerKeyUp}/>
+            <h2>입력 내용 » {message}</h2>
+            <h2>사용자 이름 » {username}</h2>
+            <button onClick={handlerClick}>확인</button>
 
-
-    handlerClick = () => this.setState({message: "", username: ""});
-    handlerKeyUp = e => {
-        console.log(e);
-        if (e.key === 'Enter') {
-            this.handlerClick();
-        }
-    }
-
-    render() {
-        return (
-            <>
-                <h1>이벤트 연습</h1>
-                <input type="text" name="message" placeholder="입력해 보세요." 
-                    value={this.state.message} onChange={this.handlerChange} onKeyUp={this.handlerKeyUp}/>
-                <input type="text" name="username" placeholder="사용자 이름을 입력하세요."
-                    value={this.state.username} onChange={this.handlerChange} onKeyUp={this.handlerKeyUp}/>
-                <h2>입력 내용 » {this.state.message}</h2>
-                <h2>사용자 이름 » {this.state.username}</h2>
-                <button onClick={this.handlerClick}>확인</button>
-            </>
-        );
-    }
+        </>
+    )
 }
 
 export default EventPractice;
