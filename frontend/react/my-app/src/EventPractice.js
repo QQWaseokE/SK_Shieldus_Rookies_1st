2cbe2a69a1d493750d02665d2439201b1fbc45ed
QQWaseokE <waseoke@gmail.com>
2024-07-02 21:55:28 +0900

    하나의 이벤트 핸들러로 통합 ⇒ e.target.name을 활용
    7/2(화)

--- frontend/react/my-app/src/EventPractice.js
+++ frontend/react/my-app/src/EventPractice.js
@@ -4,8 +4,17 @@ const EventPractice = () => {
     const [ message, setMessage ] = useState('');
     const [ username, setUsername ] = useState('');
 
-    const handlerChangeMessage = e => setMessage(e.target.value);
-    const handlerChangeUsername = e => setUsername(e.target.value);
+    // const handlerChangeMessage = e => setMessage(e.target.value);
+    // const handlerChangeUsername = e => setUsername(e.target.value);
+
+    const handlerChange = e => {
+        if (e.target.name === 'message') {
+            setMessage(e.target.value);
+        }
+        if (e.target.name === 'username') {
+            setUsername(e.target.value);
+        }
+    }
 
     const handlerClick = () => {
         setMessage('');
@@ -22,9 +31,9 @@ const EventPractice = () => {
         <>
             <h1>이벤트 연습</h1>
             <input type="text" name="message" placeholder="입력해 보세요." 
-                value={message} onChange={handlerChangeMessage} onKeyUp={handlerKeyUp} />
+                value={message} onChange={handlerChange} onKeyUp={handlerKeyUp} />
             <input type="text" name="username" placeholder="사용자 이름을 입력하세요."
-                value={username} onChange={handlerChangeUsername} onKeyUp={handlerKeyUp}/>
+                value={username} onChange={handlerChange} onKeyUp={handlerKeyUp}/>
             <h2>입력 내용 » {message}</h2>
             <h2>사용자 이름 » {username}</h2>
             <button onClick={handlerClick}>확인</button>
