b525b23980981a48629ce21229a72e68092b98c6
QQWaseokE <waseoke@gmail.com>
2024-07-02 22:20:05 +0900

    #1 리스트 항목을 id와 text로 분리하고, 각각을 관리하는 state 변수를 추가
    7/2(화)

--- frontend/react/my-app/src/IterationSample.js
+++ frontend/react/my-app/src/IterationSample.js
@@ -1,7 +1,14 @@
+import { useState } from "react";
+
 const IterationSample = () => {
-    const names = ["눈사람", "얼음", "눈", "바람"];
-    const nameList = names.map((name, index)=><li key={index}>{name}</li>);
-    console.log(nameList);
+    const [names, setNames] = useState([
+        { id:1, text:"눈사람"},
+        { id:2, text:"얼음"},
+        { id:3, text:"눈"},
+        { id:4, text:"바람"}
+    ]);
+
+    const nameList = names.map(name=><li key={name.id}>{name.text}</li>);
     return (
         <>
             <ul>{nameList}</ul>
